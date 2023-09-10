62df34ff186b916a8f2777e4a5b7d45d02101eda
MadMoth125 <avery.j.sims@gmail.com>
2023-09-10 00:10:41 -0500

    Copied project to new directory

---
+++ LibraryCatalogSystem/LibraryCatalogSystem/Book.cs
@@ -0,0 +1,52 @@
+﻿namespace LibraryCatalogSystem
+{
+	public enum BookStatus
+	{
+		Available,
+		CheckedOut
+	}
+
+	public class Book
+	{
+		// Properties
+		public ulong ISBN { get; set; }
+		public string Title { get; set; }
+		public string Author { get; set; }
+		public BookStatus Status { get; set; }
+
+		// Constructor
+		public Book(string title, string author, BookStatus status)
+		{
+			// ISBN is currently not saved in the constructor,
+			// but instead is used as a key in the dictionary
+			Title = title;
+			Author = author;
+			Status = status;
+		}
+
+		// The original ISBN keys were 13 digits long and it was annoying to type them out constantly,
+		// so I've shortened them down to 4 digits for convenience while demonstrating the program.
+		public static Dictionary<ulong, Book> PresetLibrary { get; private set; } = new Dictionary<ulong, Book>
+		{
+			{ 0084, new Book("To Kill a Mockingbird", "Harper Lee", BookStatus.Available) },
+			{ 9970, new Book("Pride and Prejudice", "Jane Austen", BookStatus.CheckedOut) },
+			{ 6225, new Book("The Da Vinci Code", "Dan Brown", BookStatus.Available) },
+			{ 3415, new Book("The Hobbit", "J.R.R. Tolkien", BookStatus.Available) },
+			{ 0803, new Book("1984", "George Orwell", BookStatus.CheckedOut) },
+			{ 4113, new Book("The Catcher in the Rye", "J.D. Salinger", BookStatus.Available) },
+			{ 2719, new Book("Dune", "Frank Herbert", BookStatus.CheckedOut) },
+			{ 0524, new Book("The Great Gatsby", "F. Scott Fitzgerald", BookStatus.Available) }
+			/* original ISBN book keys
+			 *  9780061120084
+				9780141439970
+				9780739326225
+				9780544003415
+				9780141980803
+				9781400064113
+				9780441172719
+				9780060850524
+			*/
+		};
+		
+	}
+}
---
+++ LibraryCatalogSystem/LibraryCatalogSystem/Program.cs
@@ -0,0 +1,103 @@
+﻿using CustomConsole;
+using GenericParse;
+
+namespace LibraryCatalogSystem
+{
+	internal class Program
+	{
+		public static Dictionary<ulong, Book> Catalog = Book.PresetLibrary;
+
+		private static string[] menu1 = { "View all books", "Search by ISBN", "Search by Title", "Search by Author", "Check out a book", "Return a book" };
+		private static string[] menu2 = { "Exit program" };
+
+		private static bool _loopMain = true;
+		
+		static void Main(string[] args)
+		{
+			while (_loopMain)
+			{
+				PrintMenu();
+				SelectMenuOption();
+			}
+		}
+
+		/// <summary>
+		/// Displays all menu options in the console.
+		/// </summary>
+		static void PrintMenu()
+		{
+			Console.WriteLine("Welcome to the smallest Library Catalog");
+			ConsoleHelper.PrintBlank();
+			ConsoleHelper.PrintStrings( new[] {menu1, menu2} );
+		}
+
+		/// <summary>
+		/// Waits for user input and calls SwitchOnMenuSelection(), passing the user's input as a parameter.
+		/// </summary>
+		private static void SelectMenuOption()
+		{
+			// looping until a valid option is selected
+			while (true)
+			{
+				ConsoleHelper.PrintBlank();
+				Console.Write("Select option: ");
+				int tempSelect = GenericReadLine.TryReadLine<int>();
+
+				if (!SwitchOnMenuSelection(tempSelect))
+				{
+					break;
+				}
+			}
+		}
+		
+		/// <summary>
+		/// Uses a switch statement to call the appropriate method based on the user's menu selection.
+		/// </summary>
+		/// <param name="selection">The user's menu selection</param>
+		/// <returns>The desired loop state</returns>
+		private static bool SwitchOnMenuSelection(int selection)
+		{
+			bool tempReturnValue = true;
+
+			// clearing console and printing menu again to prevent clutter
+			Console.Clear();
+			PrintMenu();
+			ConsoleHelper.PrintBlank();
+
+			switch (selection)
+			{
+				case 1: // View all books
+					SearchManager.PrintBooks(Catalog);
+					break;
+				case 2: // Search by ISBN
+					Console.Write("Search for a book by ISBN: ");
+					SearchManager.SearchByISBN(GenericReadLine.TryReadLine<ulong>(), Catalog, out Dictionary<ulong, Book> resultsISBN);
+					break;
+				case 3: // Search by Title
+					Console.Write("Search for a book via title: ");
+					SearchManager.SearchByTitle(GenericReadLine.TryReadLine<string>(), Catalog, out Dictionary<ulong, Book> resultsTitle);
+					break;
+				case 4: // Search by Author
+                    Console.Write("Search for a book via author: ");
+                    SearchManager.SearchByAuthor(GenericReadLine.TryReadLine<string>(), Catalog, out Dictionary<ulong, Book> resultsAuthor);
+					break;
+				case 5: // Check out book
+					Console.Write("Enter the ISBN of the book you wish to check out: ");
+					SearchManager.CheckOutBook(GenericReadLine.TryReadLine<ulong>(), ref Catalog);
+					break;
+				case 6: // return book
+					Console.Write("Enter the ISBN of the book you wish to return: ");
+					SearchManager.ReturnBook(GenericReadLine.TryReadLine<ulong>(), ref Catalog);
+					break;
+				case 7: // exit program
+					tempReturnValue = false;
+					_loopMain = false;
+					break;
+				default: // Invalid selection
+					ConsoleHelper.PrintInvalidSelection();
+					break;
+			}
+			return tempReturnValue;
+		}
+	}
+}
